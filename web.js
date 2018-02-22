199f932256f5038af8ef0e54c97ce795e33f57de
Takashi SASAKI <takashi316@gmail.com>
2018-02-22 22:34:38 +0900

    rare limit in doGet

--- web.js
+++ web.js
@@ -1,4 +1,30 @@
-function doGet() {
+var maxAccessCount = 100;
+var accessCountExpiresIn = 60;
+
+function doGet(e) {
+  var temporaryActiveUserKey = Session.getTemporaryActiveUserKey();
+  if(typeof CacheService.getScriptCache().get(temporaryActiveUserKey) === "string"){
+    var accessCount = parseInt(CacheService.getScriptCache().get(temporaryActiveUserKey));
+    if(accessCount > maxAccessCount) {
+      return ContentService.createTextOutput("Rate limit exceeded. " + accessCount);
+    }
+    CacheService.getScriptCache().put(temporaryActiveUserKey, "" + (accessCount + 1), accessCountExpiresIn);
+  } else {
+    CacheService.getScriptCache().put(temporaryActiveUserKey, "1", accessCountExpiresIn);
+  }
+  
+  if(typeof e.parameter.clipboardId === "string") {
+    var clipboardId = e.parameter.clipboardId;
+    if(clipboardId.length == 24) {
+      var contentTextOutput = ContentService.createTextOutput();
+      contentTextOutput.setMimeType(ContentService.MimeType.JSON);
+      contentTextOutput.setContent(JSON.stringify(get(clipboardId)));
+      return contentTextOutput;
+    } else {
+      return ContentService.createTextOutput("clipboardId should be a string of 24 characters.");
+    }
+  }
+  
   var htmlTemplate = HtmlService.createTemplateFromFile("index");
   htmlTemplate.js = getJsdoit("js");
   htmlTemplate.css = getJsdoit("css");
@@ -7,6 +33,10 @@ function doGet() {
   return htmlOutput;
 }
 
+function doPost(e){
+  Logger.log(e);
+  return doGet(e);
+}
 
 function getJsdoit(kind){
   var js = CacheService.getScriptCache().get(kind);
