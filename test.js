cfe7a1b252770509cb22727dc493b92e1ada382d
Takashi SASAKI <takashi316@gmail.com>
2018-03-08 00:10:08 +0900

    reset between put and get in some tests

--- test.js
+++ test.js
@@ -4,22 +4,26 @@ function testLazyKeyValueStore(){
   lkvs.roundtripTest("k", null); 
   lkvs.roundtripTest("k", null, true); 
   lkvs.roundtripTest("emptyString", "");
+  lkvs.roundtripTest("emptyString", "", true);
   lkvs.roundtripTest("booleanTrue", true);
+  lkvs.roundtripTest("booleanTrue", true, true);
   lkvs.roundtripTest("booleanFalse", false);
+  lkvs.roundtripTest("booleanFalse", false, true);
   lkvs.roundtripTest("number", 1.234E6);
-  lkvs.roundtripTest("longString",  "aosifjdajasiopfjdsajioasfopsiadfsajasd:alnvuipaojvdaslfhuiaojask;fcmuioa:kscdasnpiuacjaso");
-  lkvs.roundtripTest("emptyString", "");
+  lkvs.roundtripTest("number", 1.234E6, true);
+  lkvs.roundtripTest("longString",  "aosifjdajasiopfjdsajioasfopsiadfsajasd:alnvuipaojvdaslfhuiaojask;fcmuioa:kscdasnpiuacjaso", true);
+  lkvs.roundtripTest("emptyString", "", true);
   lkvs.roundtripTest("testObject1", {
     a: 1,
     b: null,
     c: "hello",
     d: "oajsfioajfisdajfasdjfdaajiosfpiohruiaghruipoajeofjrghriopajgrioahiogjopefjeriopajgekop:ajbuipagojerwasgbruipoa;jfvhraeuighrewgihuiopagrhj"
-  });
+  }, true);
   lkvs.roundtripTest("testObject1", {
     a: 1,
     b: null,
     c: "hello"
-  });
+  }, true);
   lkvs.roundtripTest("testObject3", {
     aaa : 1,
     bbb : "こんにちは",
@@ -27,14 +31,14 @@ function testLazyKeyValueStore(){
     1 : 111,
     2 : 222,
     3 : 333
-  });  
-  lkvs.roundtripTest("testObject4", {a:"aaa", b:"bbb", 1:0.111, 2:0.222});
-  lkvs.roundtripTest("testObject5", {a:null, b:false});
-  lkvs.roundtripTest("testObject6", {aaa: 1111, bbb: "2222", ccc: null});
-  lkvs.roundtripTest("testArray1",  [1, 2, 3, "a", "b", "c"]);
-  lkvs.roundtripTest("testArray2", []);
-  lkvs.roundtripTest("testArray3", [1,2,3]);
-  lkvs.appendArray("testArray3", [4,5,6]);
-  assert.deepStrictEqual(lkvs.get("testArray3"), [1,2,3,4,5,6]);
-  lkvs.roundtripTest("testObject7", { a : 1, b: null, c: false, d: [ "hello", 1.23, null]});
+  }, true);  
+  lkvs.roundtripTest("testObject4", {a:"aaa", b:"bbb", 1:0.111, 2:0.222}, true);
+  lkvs.roundtripTest("testObject5", {a:null, b:false}, true);
+  lkvs.roundtripTest("testObject6", {aaa: 1111, bbb: "2222", ccc: null}, true);
+  lkvs.roundtripTest("testArray1",  [1, 2, 3, "a", "b", "c"], true);
+  lkvs.roundtripTest("testArray2", [], true);
+  lkvs.roundtripTest("testArray3", [1,2,3], true);
+  lkvs.appendArray("testArray3", [4,5,6], true);
+  assert.deepStrictEqual(lkvs.get("testArray3"), [1,2,3,4,5,6], true);
+  lkvs.roundtripTest("testObject7", { a : 1, b: null, c: false, d: [ "hello", 1.23, null]}, true);
 }
