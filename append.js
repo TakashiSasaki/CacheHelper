28200eb2ae6f2065a214eeb1dc4ac3c60b48011d
Takashi SASAKI <takashi316@gmail.com>
2018-03-07 23:20:53 +0900

    remove old comment lines

--- append.js
+++ append.js
@@ -3,20 +3,12 @@
  * @param {array} array
 */
 function appendArray_(key, array) {
-　assert(typeof key === "string");
+  assert(typeof key === "string");
   assert(array instanceof Array);
   var currentLength = parseInt(this.read("[" + key + "]"));
   assert(typeof currentLength === "number");
   this.write("[" + key + "]", "" + (currentLength + array.length));
-//  var all = {};
-//  all["[" + key + "]"] = "" + (parseInt(l) + array.length);
   for(var i=0; i<array.length; ++i) {
     this.put("[" + key + "]"  + (currentLength + i), array[i]);
-    //merge_(all, putAny("[" + key + "]" + (parseInt(l) + i), array[i]));
   }//for i
-//  if(typeof all["TO BE REMOVED"] !== "undefined") {
-//    cache.removeAll(all["TO BE REMOVED"]);
-//    delete all["TO BE REMOVED"];
-//  }
-//  cache.putAll(all);
 }//appendArray
