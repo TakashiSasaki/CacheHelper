53b06915e3981bb3f10bc402a4f3a8a2f416ade9
Takashi SASAKI <takashi316@gmail.com>
2018-03-07 22:26:36 +0900

    remove unused files

--- misc.js
+++
@@ -1,7 +0,0 @@
-//if(typeof global === "undefined") global = this;
-//if(global.cache === undefined) global.cache = CacheService.getScriptCache();
-
-//function getDerivedKeys_(key){
-//  return ["$" + key + "$", "(" + key + ")", "[" + key + "]", "{" + key + "}", "#" + key + "#"];
-//}
-
--- modKey.js
+++
@@ -1,5 +0,0 @@
-//function getDerivedKeys_(key){
-//function modKey(key) {
-//  assert(typeof key === "string");
-//  return ["$" + key + "$", "(" + key + ")", "[" + key + "]", "{" + key + "}", "#" + key + "#"];  
-//}
